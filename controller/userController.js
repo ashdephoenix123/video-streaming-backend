20aca8e60636278a36bc4989dd163884f538289c
Akash <akash@appycodes.com>
2025-07-31 10:42:51 +0530

    updated cookie serialization option

--- controller/userController.js
+++ controller/userController.js
@@ -75,7 +75,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const serialized = serialize("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV == "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 60 * 60 * 24,
     path: "/",
   });

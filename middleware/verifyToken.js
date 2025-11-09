6ba2ec0f3d3e7aad454a7cd2e222cd7fe05e6907
ashdephoenix123 <akashsarki12345@gmail.com>
2025-11-09 10:06:50 +0530

    middleware updated

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,21 +3,25 @@ const jwt = require("jsonwebtoken");
 const jwt_secret = process.env.JWT_SECRET;
 
 const verifyToken = asyncHandler(async (req, res, next) => {
-  console.log("req.cookies", req.cookies);
-  console.log("req.cookies.token", req.cookies.token);
-  console.log("req.headers", req.headers);
-  const token = req.cookies.token;
+  let token;
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    try {
+      token = authHeader.split(" ")[1];
+      const decoded = jwt.verify(token, jwt_secret);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      console.error("Token verification error:", error.message);
+      res.status(401);
+      throw new Error("Token invalid or expired!");
+    }
+  }
+
   if (!token) {
     res.status(401);
     throw new Error("Auth token not provided!");
-  }
-  try {
-    const decoded = jwt.verify(token, jwt_secret);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(401);
-    throw new Error("Token invalid or expired!");
   }
 });
 

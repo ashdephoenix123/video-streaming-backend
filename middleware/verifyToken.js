e4eb7ea8d41bbf36b9af98aff4dd71f596df60bc
akash <akash@appycodes.com>
2026-01-29 18:00:52 +0530

    testing verifytoken

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -5,23 +5,20 @@ const ApiError = require("../utils/ApiError");
 const jwt_secret = process.env.JWT_SECRET;
 
 const verifyToken = asyncHandler(async (req, res, next) => {
-  let token;
-  const authHeader = req.headers.authorization;
-
-  if (authHeader && authHeader.startsWith("Bearer")) {
-    try {
-      token = authHeader.split(" ")[1];
-      const decoded = jwt.verify(token, jwt_secret);
-      req.user = decoded;
-      next();
-    } catch (error) {
-      console.error("Token verification error:", error.message);
-      throw new ApiError(HTTP_ERRORS.UNAUTHORIZED, "Token invalid or expired!");
-    }
-  }
+  const token =
+    req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     throw new ApiError(HTTP_ERRORS.UNAUTHORIZED, "Auth token not provided!");
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwt_secret);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error("Token verification error:", error.message);
+    throw new ApiError(HTTP_ERRORS.UNAUTHORIZED, "Token invalid or expired!");
   }
 });
 
